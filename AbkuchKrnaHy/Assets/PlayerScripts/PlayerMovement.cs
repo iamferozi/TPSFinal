2d9f2f5a9ed9b3f7064cad65d76b8ddd00835982
Danish2371 <syed_danishasif@yahoo.com>
2021-01-05 08:55:07 -0800

    Yolo

---
+++ AbkuchKrnaHy/Assets/PlayerScripts/PlayerMovement.cs
@@ -0,0 +1,129 @@
+﻿//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+
+//public class PlayerMovement : MonoBehaviour
+//{
+//    public CharacterController playerController;
+
+//    public float speed;
+//   //float gravity = 9.81f;
+
+//    Vector3 movement;
+//    //Vector3 velocity;
+
+//    //public Transform groundCheck;
+//    //public float groundDistance = 0.5f;
+//    //public LayerMask groundMask;
+//    //bool isGrounded;
+//    // Start is called before the first frame update
+//    void Start()
+//    {
+
+//    }
+
+//    // Update is called once per frame
+//    void Update()
+//    {
+//        PlayerMove();
+//    }
+
+//    public void PlayerMove()
+//    {
+//        //isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+//        //if (isGrounded && velocity.y < 0)
+//        //{
+//        //    velocity.y = -2f;
+//        //}
+
+//        float xPos = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+//        float zPos = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+
+//        movement = transform.right * xPos + transform.forward * zPos;
+
+//        playerController.Move(movement);
+
+//        //velocity.y += gravity * Time.deltaTime;
+
+//        //playerController.Move(velocity * Time.deltaTime);
+//    }
+//}
+
+
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 6f;            // The speed that the player will move at.
+
+    Vector3 movement;                   // The vector to store the direction of the player's movement.
+    Animator anim;                      // Reference to the animator component.
+    Rigidbody playerRigidbody;          // Reference to the player's rigidbody.
+    int floorMask;                      // A layer mask so that a ray can be cast just at gameobjects on the floor layer.
+    float camRayLength = 100f;          // The length of the ray from the camera into the scene.
+
+    void Awake()
+    {
+        // Create a layer mask for the floor layer.
+        floorMask = LayerMask.GetMask("Ground");
+
+        // Set up references.
+        anim = GetComponent<Animator>();
+        playerRigidbody = GetComponent<Rigidbody>();
+    }
+
+
+    void FixedUpdate()
+    {
+        // Store the input axes.
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+
+        // Move the player around the scene.
+        Move(h, v);
+
+        // Turn the player to face the mouse cursor.
+        Turning();
+
+        // Animate the player.
+        //Animating(h, v);
+    }
+
+    void Move(float h, float v)
+    {
+        // Set the movement vector based on the axis input.
+        movement.Set(h, 0f, v);
+
+        // Normalise the movement vector and make it proportional to the speed per second.
+        movement = movement.normalized * speed * Time.deltaTime;
+
+        // Move the player to it's current position plus the movement.
+        playerRigidbody.MovePosition(transform.position + movement);
+    }
+
+    void Turning()
+    {
+        // Create a ray from the mouse cursor on screen in the direction of the camera.
+        Ray camRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        // Create a RaycastHit variable to store information about what was hit by the ray.
+        RaycastHit floorHit;
+
+        // Perform the raycast and if it hits something on the floor layer...
+        if (Physics.Raycast(camRay, out floorHit, camRayLength, floorMask))
+        {
+            // Create a vector from the player to the point on the floor the raycast from the mouse hit.
+            Vector3 playerToMouse = floorHit.point - transform.position;
+
+            // Ensure the vector is entirely along the floor plane.
+            playerToMouse.y = 0f;
+
+            // Create a quaternion (rotation) based on looking down the vector from the player to the mouse.
+            Quaternion newRotation = Quaternion.LookRotation(playerToMouse);
+
+            // Set the player's rotation to this new rotation.
+            playerRigidbody.MoveRotation(newRotation);
+        }
+    }
+}
---
+++ AbkuchKrnaHy/Assets/PlayerScripts/Teleportation.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleportation : MonoBehaviour
+{
+    public GameObject prefab;
+    //public float distance;
+    public Transform playerPos;
+
+    GameObject teleport;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+        Teleport();
+        Dash();
+    }
+    void Update()
+    {
+
+    }
+    private void OnMouseDown()
+    {
+        Vector3 newPos = playerPos.position + new Vector3(0f, 0f, 2f);
+        GameObject telePref = Instantiate(prefab, newPos, Quaternion.identity) as GameObject;
+
+        teleport = telePref;
+    }
+    public void Teleport()
+    {
+        if (Input.GetKey(KeyCode.Q))
+        {
+            if (teleport != null)
+            {
+                playerPos.transform.position = teleport.transform.position;
+            }
+
+        }
+    }
+
+    public void Dash()
+    {
+        if (Input.GetKey(KeyCode.E))
+        {
+            Vector3 newPos = new Vector3(0f, 0f, (playerPos.position.z + 2f));
+            playerPos.transform.position = newPos;
+
+        }
+    }
+
+}
